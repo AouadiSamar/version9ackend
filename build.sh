afc0f7586981f27c03234edc300c8a9ac72cd7a9
AouadiSamar <samaraouadi7@gmail.com>
2024-06-08 01:53:19 +0200

    Update build configuration

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# Utiliser Python et Pip directement
-python3 -m pip install --upgrade pip setuptools wheel
-python3 -m pip install -r requirements.txt
+# Install dependencies
+pip install -r requirements.txt
 
-# Appliquer les migrations de base de données
-python3 manage.py migrate
+# Run migrations
+python manage.py migrate
 
-# Collecter les fichiers statiques
-python3 manage.py collectstatic --noinput
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Any other build steps
