3347fd484c3bbbff7847ba153d2fc158b10949fb
AouadiSamar <samaraouadi7@gmail.com>
2024-06-08 00:57:31 +0200

    Your commit message

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\samar\Desktop\projetPFEE\projetPFE77\projetPFE\myenv
+set VIRTUAL_ENV=C:\Users\samar\Version5\Baackend\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
