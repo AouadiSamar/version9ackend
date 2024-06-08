ab0e17489cf209f132868f3e1964e7ce07e4a74a
AouadiSamar <samaraouadi7@gmail.com>
2024-06-08 02:32:53 +0200

    Add constraints, requirements, vercel config, anbt

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
-python3 -m pip install -r requirements.txt
-python3 manage.py migrate
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
 python3 manage.py collectstatic --noinput
+python3 manage.py migrate
