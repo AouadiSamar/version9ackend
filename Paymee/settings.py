8b5c0d098bdc848526e92d0830bb501d7bc023c4
AouadiSamar <samaraouadi7@gmail.com>
2024-06-13 14:06:07 +0200

    h

--- Paymee/settings.py
+++ Paymee/settings.py
@@ -6,25 +6,72 @@ import dj_database_url
 
 env = environ.Env(DEBUG=(bool, False))
 
-# Construire les chemins de base
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Lecture du fichier .env
+# Reading .env file
 environ.Env.read_env(BASE_DIR / ".env")
 
-# Configuration de base
+# Quick-start development settings - unsuitable for production
 DEBUG = env("DEBUG")
 SECRET_KEY = env("SECRET_KEY")
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+ALLOWED_HOSTS = ["*"]
 
-# Configuration de la base de données
+# Application definition
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'chargebacks.apps.ChargebacksConfig',
+    'rembourssement.apps.RembourssementConfig',
+    'users.apps.UsersConfig',  # Updated to reference the configuration class
+    'djoser',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+ROOT_URLCONF = 'Paymee.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'Paymee.wsgi.application'
+
 DATABASES = {
     'default': dj_database_url.config(
-        default=env('DATABASE_URL')
+        default=os.environ.get('DATABASE_URL')
     )
 }
 
-# Configuration des e-mails
+# Email configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = env('EMAIL_HOST')
 EMAIL_PORT = env('EMAIL_PORT')
@@ -33,13 +80,13 @@ EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = env('EMAIL_HOST_USER')
 
-# Configuration des sessions
+# Session configuration
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 SESSION_COOKIE_AGE = 5000
 SESSION_SAVE_EVERY_REQUEST = True
 
-# Configuration REST Framework et JWT
+# REST Framework and JWT configuration
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
@@ -62,10 +109,10 @@ SIMPLE_JWT = {
     'JTI_CLAIM': 'jti',
 }
 
-# Modèle utilisateur personnalisé
+# Custom user model
 AUTH_USER_MODEL = 'users.User'
 
-# Validation des mots de passe
+# Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -81,21 +128,25 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Internationalisation
+# Internationalization
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-# Fichiers statiques et médias
+# Static files
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Configuration de logging
+# Default primary key field type
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Logging configuration
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -113,7 +164,7 @@ LOGGING = {
     },
 }
 
-# Configuration de Djoser
+# Djoser configuration
 DOMAIN = 'localhost:5173'
 DJOSER = {
     'LOGIN_FIELD': 'email',
@@ -135,9 +186,8 @@ DJOSER = {
     },
 }
 
-# Nom du site
+# Site name
 SITE_NAME = "Paymee"
-
 # Configuration CORS
 CORS_ALLOWED_ORIGINS = [
     'https://samar-cdd4a.web.app',  # URL de votre application déployée sur Firebase
